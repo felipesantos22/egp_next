96d7c52df90cd1296706eb7fc965ab15968f1793
felipesantos22 <felipevs2018@gmail.com>
2026-03-03 17:58:03 -0300

    change bg home

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="br">
-      <body className={`${inter.className} bg-gray-50`}>
+      <body className={`${inter.className}`}>
         <Header />
         {children}
         <Footer />

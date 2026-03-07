a4751dfb2fedd18871e27001de08795d9b33c436
felipesantos22 <felipevs2018@gmail.com>
2026-03-07 09:20:37 -0300

    insert overflow

--- components/table-egp-4-dias.tsx
+++ components/table-egp-4-dias.tsx
@@ -1,8 +1,13 @@
 export default function TableEgp4Dias() {
   return (
-    <section className=" bg-gray-50 py-10 mx-10 my-10">
-      <h2 className="text-center mb-5 text-3xl underline">Hotéis Previstos ou similares</h2>
+    <section className=" bg-gray-50 py-10 mx-10 my-10 overflow-x-auto">
+      <h2 className="text-center mb-5 text-3xl underline">
+        Hotéis Previstos ou similares
+      </h2>
       <table className="border border-gray-300 w-full">
+        <caption className="caption-top">
+          Table 3.1: Professional wrestlers and their signature moves.
+        </caption>
         <thead>
           <tr className="bg-gray-100">
             <th className="border px-4 py-2">Cidade</th>
@@ -15,9 +20,39 @@ export default function TableEgp4Dias() {
         <tbody>
           <tr className="text-center">
             <td className="border px-4 py-2">Cairo</td>
-            <td className="border px-4 py-2">Cleópatra<br/>Pyramids Park<br/>Barceló<br/>Oásis<br/>ou similares</td>
-            <td className="border px-4 py-2">Ramsis<br/>Safir Dokki<br/>Novotel<br/>Radisson Blue<br/>ou similares</td>
-            <td className="border px-4 py-2">Conrad<br/>Fairmont<br/>Steigenburguer<br/>Pyramides<br/>ou similares</td>
+            <td className="border px-4 py-2">
+              Cleópatra
+              <br />
+              Pyramids Park
+              <br />
+              Barceló
+              <br />
+              Oásis
+              <br />
+              ou similares
+            </td>
+            <td className="border px-4 py-2">
+              Ramsis
+              <br />
+              Safir Dokki
+              <br />
+              Novotel
+              <br />
+              Radisson Blue
+              <br />
+              ou similares
+            </td>
+            <td className="border px-4 py-2">
+              Conrad
+              <br />
+              Fairmont
+              <br />
+              Steigenburguer
+              <br />
+              Pyramides
+              <br />
+              ou similares
+            </td>
           </tr>
         </tbody>
       </table>

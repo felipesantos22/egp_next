3b2463fae1c366f9f00920b52824aab25952870e
felipesantos22 <felipevs2018@gmail.com>
2026-03-20 17:15:37 -0300

    insert whatsapp

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+
 export default function Footer() {
+  const numero = "5511997666641";
+  const mensagem = "Olá vim pelo site!";
+
+  const link = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
   return (
-    <footer className="bg-gray-800 text-white h-32 flex items-center justify-center">
-      <p>&copy; 2026 Egitour Viagens. Todos os direitos reservados.</p>
+    <footer className="bg-black text-white h-32 flex flex-col items-center justify-center gap-2">
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <Image
+          src="/whats.png"
+          alt="WhatsApp"
+          width={32}
+          height={32}
+          className="hover:scale-110 transition"
+        />
+      </a>
+
+      <p>&copy; 2026 Egito Viagens. Todos os direitos reservados.</p>
     </footer>
   );
 }

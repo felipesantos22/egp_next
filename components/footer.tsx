4b2820ee8e52f9d3631d33dad73560252cde52fc
felipesantos22 <felipevs2018@gmail.com>
2026-03-20 16:49:45 -0300

    change font family

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white h-32 flex items-center justify-center">
-      <p>&copy; 2026 Egito Viagens. Todos os direitos reservados.</p>
+      <p>&copy; 2026 Egitour Viagens. Todos os direitos reservados.</p>
     </footer>
   );
 }
